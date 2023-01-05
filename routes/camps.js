eff8dabca651a17a47bbaecfd35699b2a2fa44b4
ssemlitz <sdsemlitz@yahoo.com>
2023-01-05 13:02:13 -0500

    update routes

--- routes/camps.js
+++ routes/camps.js
@@ -3,17 +3,19 @@ import { Campground } from "../models/campground.js"
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.render('home')
-})
-
 router.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({})
   res.render('campgrounds/index', { campgrounds })
 })
 
-router.get('/campgrounds.new', (req, res) => {
+router.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new')
+})
+
+router.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground)
+  await campground.save()
+  res.redirect(`/campgrounds/${campground._id}`)
 })
 
 router.get('/campgrounds/:id', async (req, res) => {
@@ -21,20 +23,24 @@ router.get('/campgrounds/:id', async (req, res) => {
   res.render('campgrounds/show', { campground })
 })
 
-router.get('/campgrounds/:id/edit'), async (req, res) => {
-
-}
-
-router.put('/campgrounds/:id', async (req,res) => {
-  const {id} = req.params
-  Campground.fin
+router.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/edit', { campground })
 })
 
-router.post('/campgrounds', async (req,res) => {
-  const campground = new Campground(req.body.campground)
-  await campground.save()
-  res.redirect(`/campgrounds/${campground._id`)
+router.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+  res.redirect(`/campgrounds/${campground._id}`)
 })
+
+router.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params
+  const campground = await Campground.findByIdAndDelete(id)
+  res.redirect('/campgrounds')
+})
+
+
 
 
 
